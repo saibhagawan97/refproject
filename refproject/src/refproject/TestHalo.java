87a463dc6afaae01c7d7637efae2fed328a141a2
neojobhunter001 <neojobhunter001@gmail.com>
2021-10-27 21:46:41 +0530

    new again to refproject

---
+++ refproject/src/refproject/TestHalo.java
@@ -0,0 +1,10 @@
+package refproject;
+
+public class TestHalo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo from Java Test");
+	}
+
+}
